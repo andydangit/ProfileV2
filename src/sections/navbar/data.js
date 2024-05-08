2062097b89acaab92ec260f7db821b724d79c4b2
andydangit <andy.dang.it@gmail.com>
2024-05-07 20:53:17 -0500

    Add data list file

---
+++ src/sections/navbar/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {id: 1, link: '#', title: 'Home'},
+    {id: 1, link: '#', title: 'Home'},
+    {id: 1, link: '#', title: 'Home'},
+    {id: 1, link: '#', title: 'Home'},
+    {id: 1, link: '#', title: 'Home'},
+
+]
+
+export default data;
