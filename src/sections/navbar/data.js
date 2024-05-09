9d191b4cfab1f045446789ee9229b7e6df8a9c9d
andydangit <andy.dang.it@gmail.com>
2024-05-08 21:19:02 -0500

    add name to the title

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -1,9 +1,9 @@
 const data = [
     {id: 1, link: '#', title: 'Home'},
-    {id: 1, link: '#', title: 'Home'},
-    {id: 1, link: '#', title: 'Home'},
-    {id: 1, link: '#', title: 'Home'},
-    {id: 1, link: '#', title: 'Home'},
+    {id: 2, link: '#about', title: 'About'},
+    {id: 3, link: '#testimonials', title: 'Testimonials'},
+    {id: 4, link: '#project', title: 'Project'},
+    {id: 5, link: '#contact', title: 'Contact'},
 
 ]
 
