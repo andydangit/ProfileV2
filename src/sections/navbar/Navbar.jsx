4f369aa02d63f61fa585355a91971e33319da341
andydangit <andy.dang.it@gmail.com>
2024-05-08 21:24:03 -0500

    map item from data to the navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 import "./navbar.css";
 
-import Logo from '../../assets/logo.png'
+import Logo from "../../assets/logo.png";
+import data from "./data";
 
 function Navbar() {
   return (
     <nav>
       <div className="container nav_Container">
-        <a href=""> 
-         <img src={Logo} alt="" /> 
+        <a href="">
+          <img src={Logo} alt="Logo" />
         </a>
-        <ul></ul>
+        <ul className="nav_menu">
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
         <button></button>
       </div>
     </nav>
