86f5e18b358ca31526151b7b716d849695c5c0fa
andydangit <andy.dang.it@gmail.com>
2024-05-09 10:28:06 -0500

    Add profile pic, name, info

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,30 @@
-import './header.css'
+import "./header.css";
+
+import headerImag from "../../assets/headerPic.jpg";
 
 function Header() {
   return (
-    <header id="header"><h2>Header</h2></header>
-  )
+    <header id="header">
+      <div className="container header_container"></div>
+      <div className="header_profile">
+        <img src={headerImag} alt=" Portrait Header" />
+        <h3> Andy Dang </h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
+          molestias nam dolores. Facere.
+        </p>
+        <div className="header_cta">
+          <a href="#contact"> Contact me</a>
+          <a href="#portfolio"> My Project </a>
+        </div>
+        <div className="header_socials">
+          {
+            
+          }
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
