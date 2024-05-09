44e21e9da82d2454d8a3a6566ff08464fc1dc637
andydangit <andy.dang.it@gmail.com>
2024-05-09 10:36:03 -0500

    Add link to the data in the header section

---
+++ src/sections/header/data.js
@@ -0,0 +1,11 @@
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
+const data = [
+  { id: 1, link: "https://www.x.com/andydangit", icon: <AiOutlineTwitter /> },
+  { id: 2, link: "https://www.linkedin.com/in/andy-dang-12693341/", icon: <AiOutlineLinkedin /> },
+  { id: 3, link: "https://github.com/andydangit", icon: <AiFillGithub /> },
+];
+
+export default data;
