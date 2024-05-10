f53ab1a8fed5f8e25ad407603f3abb889167d335
andydangit <andy.dang.it@gmail.com>
2024-05-10 03:50:53 -0500

    Add: data array to contact

---
+++ src/sections/contact/data.js
@@ -0,0 +1,10 @@
+import { HiOutlineMail } from "react-icons/hi";
+import { AiOutlineLinkedin } from "react-icons/ai";
+
+const data = [
+    {id: 1, icon: <HiOutlineMail />, link: 'mailto:andy.dang.it@gmail'},
+    {id: 2, icon: <AiOutlineLinkedin />, link: 'https://www.linkedin.com/in/andy-dang-12693341/'}
+
+]
+
+export default data
