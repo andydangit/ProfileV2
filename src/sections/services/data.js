6b6e8d79ccd6b67bc9909a4e4fd11c44191f4553
andydangit <andy.dang.it@gmail.com>
2024-05-09 21:40:39 -0500

    Add: data array to the data section

---
+++ src/sections/services/data.js
@@ -0,0 +1,26 @@
+import { FaMicrosoft } from "react-icons/fa";
+import { SiComptia } from "react-icons/si";
+import { GrCertificate } from "react-icons/gr";
+
+const data = [
+  {
+    id: 1,
+    icon: <FaMicrosoft />,
+    title: "Microsoft",
+    desc: "MCTS Window 7, MCTS Server 2008, MCTS Server 2008 Network, Azure Fundamentals",
+  },
+  {
+    id: 2,
+    icon: <SiComptia />,
+    title: "Comptia",
+    desc: "A+, Network+, Security+",
+  },
+  {
+    id: 3,
+    icon: <GrCertificate />,
+    title: "Other Certifications",
+    desc: "ITIL V3, Nucamp: Full Stack Web and Mobile Development",
+  },
+];
+
+export default data;
