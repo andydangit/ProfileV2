b61f91282f31f95fcf2b1625f981dbcaf2d446a4
andydangit <andy.dang.it@gmail.com>
2024-05-09 20:21:02 -0500

    Add: data file to the about folder

---
+++ src/sections/about/data.js
@@ -0,0 +1,9 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+
+const data = [
+    {id: 1, icon: <FaAward/>, title: "Experience", desc: '?? working experience'},
+    {id: 2, icon: <TbBooks/>, title: "Projects", desc: 'projects'},
+]
+
+export default data;
