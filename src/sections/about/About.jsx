059e7ae21ec4784df167f73eeae17ce1dc54291c
andydangit <andy.dang.it@gmail.com>
2024-05-09 20:55:23 -0500

    Bug: remove the div below

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -9,42 +9,35 @@ import data from "../about/data";
 function About() {
   return (
     <section id="about">
-      <div className="container about_container">
+    <div className="container about_container">
         <div className="about_left">
-          <div className="about_portrait">
-            <img src={AboutImage} alt="About pic" />
-          </div>
+            <div className="about_portrait">
+                <img src={AboutImage} alt="About pic" />
+            </div>
         </div>
-        <div className="about_right"></div>
-        <h2>About Me</h2>
-        <div className="about_cards">
-          {data.map((item) => (
-            <Card key={item.id} className="about_card">
-              <span className="about_card-icon">{item.icon}</span>
-              <h5>{item.title}</h5>
-              <small>{item.desc}</small>
-            </Card>
-          ))}
+        <div className="about_right">
+            <h2>About Me</h2>
+            <div className="about_cards">
+                {
+                    data.map(item => (
+                        <Card key={item.id} className="about_card">
+                            <span className='about_card-icon'>{item.icon}</span>
+                            <h5>{item.title}</h5>
+                            <small>{item.desc}</small>
+                        </Card>
+                    ))
+                }
+            </div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam asperiores sapiente quod quia tenetur culpa. Eligendi autem minus officia consequuntur!
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quasi, commodi ullam animi sapiente corrupti, laborum iure, necessitatibus quas nemo saepe ipsa possimus a tempore assumenda? Eveniet expedita nulla labore facilis quasi, a modi eligendi!
+            </p>
+            <a href={Resume} download className='btn primary'>Download CV <HiDownload/></a>
         </div>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum quis
-          accusantium architecto et asperiores, voluptas nam in. Fugit explicabo
-          fuga neque. Laboriosam, inventore. Dicta ipsum nam dolorum quaerat.
-          Ratione, facilis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          corporis harum a beatae! Beatae sit ut voluptate eveniet dolorum
-          officiis aliquam molestias assumenda quia hic distinctio officia natus
-          repellat reiciendis possimus dicta, minus alias.
-        </p>
-        <a href={Resume} download className="btn primary">
-          Download CV
-          <HiDownload />
-        </a>
-      </div>
-    </section>
+    </div>
+</section>
   );
 }
 
