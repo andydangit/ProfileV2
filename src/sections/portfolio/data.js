66a9af3480bb47ede00c4201e8451cefe82b1f72
andydangit <andy.dang.it@gmail.com>
2024-05-09 23:03:29 -0500

    Add: data array for the portfolio page

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,50 @@
+import project1 from "../../assets/project_1.png"
+import project2 from "../../assets/project_2.png"
+import project3 from "../../assets/project_3.png"
+import project4 from "../../assets/project_4.png"
+
+
+const data = [
+{
+    id: 1, 
+    category: 'frontend',
+    image: project1,
+    title: "Cafe Dao (Frontend)",
+    desc: "Asian sandwich shop that sell vietnamese unique sandwich and coffee",
+    demo:'https://cafe-dao.netlify.app/',
+    github: 'https://github.com/andydangit/CafeDao'
+},
+
+{
+    id: 2, 
+    category: 'frontend',
+    image: project2,
+    title: "ToDo List with Api Quote (Frontend)",
+    desc: "A to do list that auto generate motivational quotes using api",
+    demo:'https://andydangit.github.io/Exercise_List/',
+    github: 'https://github.com/andydangit/React-Quiz-App'
+},
+
+{
+    id: 3, 
+    category: 'frontend',
+    image: project3,
+    title: "Mandalorian Fan site (Frontend)",
+    desc: "unofficial fan site ",
+    demo:'https://mandalorian-fansite.netlify.app/home',
+    github: 'https://github.com/andydangit/mandalorianfansite'
+},
+
+{
+    id: 4, 
+    category: 'frontend',
+    image: project4,
+    title: "Vacation Spot (Frontend)",
+    desc: "Vacation spot ",
+    demo:'https://my-vacation-spots.netlify.app/',
+    github: 'https://github.com/andydangit/Vacation-Travel-Spot'
+}
+
+]
+
+export default data
