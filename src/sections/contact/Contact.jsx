ee79dde516a26e5a69f18916309e36776dc23d7b
andydangit <andy.dang.it@gmail.com>
2024-05-10 03:59:52 -0500

    Add: style to the contact

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,9 +1,19 @@
-import './contact.css'
+import "./contact.css";
+
+import contacts from './data'
 
 function Contact() {
   return (
-    <section id="contact"><h2>Contact</h2></section>
-  )
+    <section id="contact">
+      <h2>Contact</h2>
+      <p> Send me a message with any of the links below </p> 
+     <div className="container contact_container">
+{
+  contacts.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer"> {contact.icon} </a>)
+}
+     </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
